12cc5e0aa3cb419a156d5f9bca789f55ca0159fc
starlit-nox <kdkelly.1998@gmail.com>
2023-08-02 17:32:22 -0500

    added ibag thingie

--- Lab08/IBag.cs
+++ Lab08/IBag.cs
@@ -1,6 +1,10 @@
 ﻿namespace Lab08
 {
-    public interface IBag
+    public interface IBag<T> : IEnumerable<T>
     {
+        // adds the item to the bag 
+        void Pack(T item);
+        //removes the item from bag at index and returns the item that was removed
+        T Unpack(int index);
     }
 }
